1f92af5e20968a37046cb07f77e019f0d6cb2dc6
Galuh Sukma Nurul Habibah <galuh.gsnh@gmail.com>
2024-09-06 02:14:24 +0700

    new page string

--- app/src/main/java/com/galuhsukma/kalendernya/MarkdayActivity.java
+++ app/src/main/java/com/galuhsukma/kalendernya/MarkdayActivity.java
@@ -1,5 +1,7 @@
 package com.galuhsukma.kalendernya;
 
+import static com.galuhsukma.kalendernya.R.id.display_ChosenDay;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +19,7 @@ public class MarkdayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = getIntent();
         chosenDay = intent.getStringExtra("chosenDay");
-        day = findViewById(R.id.display_ChosenDay);
+        day = findViewById();
         day.setText(chosenDay);
 
     }
