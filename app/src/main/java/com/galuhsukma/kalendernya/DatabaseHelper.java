1ea6e73f1e86a2309686a8738fbb31323a62423b
gsnurh <gsnurh@gmail.com>
2025-02-21 22:46:21 +0700

    savingsqlite_markdaydone

--- app/src/main/java/com/galuhsukma/kalendernya/DatabaseHelper.java
+++ app/src/main/java/com/galuhsukma/kalendernya/DatabaseHelper.java
@@ -12,9 +12,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "HaIs";
     public static final String DB_TABLE_UTAMA = "AllMarkDay";
+    public static final String DB_TABLE_HAID = "HaidTable";
+    public static final String DB_TABLE_ISTIHADHAH = "IstihadhahTable";
     public static final int DB_VER = 1;
-    public static final String CREATE_TABLE_UTAMA = "create table "+DB_TABLE_UTAMA+" (id_tgl String);";
+    public static final String CREATE_TABLE_UTAMA =
+            "CREATE TABLE " + DB_TABLE_UTAMA + " (" +
+                    "id_tgl TEXT PRIMARY KEY, " +
+                    "jenismark TEXT, " +
+                    "waktushalat TEXT, " +
+                    "centangshalat INTEGER, " +
+                    "centangpuasa INTEGER);";
+    public static final String CREATE_TABLE_HAID = "CREATE TABLE " + DB_TABLE_HAID + " ("
+            + "id_tgl STRING PRIMARY KEY, "
+            + "jenismark TEXT NOT NULL CHECK(jenismark = 'HAID'))";
 
+    public static final String CREATE_TABLE_ISTIHADHAH = "CREATE TABLE " + DB_TABLE_ISTIHADHAH + " ("
+            + "id_tgl STRING PRIMARY KEY, "
+            + "jenismark TEXT NOT NULL CHECK(jenismark = 'ISTIHADHAH'))";
     Context ctx;
     SQLiteDatabase myDB;
 
@@ -26,7 +40,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE_UTAMA);
-        Log.i("Database", "Table Created");
+        db.execSQL(CREATE_TABLE_HAID);
+        db.execSQL(CREATE_TABLE_ISTIHADHAH);
+        Log.i("Database", "All Tables Created");
     }
 
     @Override
@@ -35,19 +51,41 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(query);
     }
 
-    public Long insertData(String tgl){
+    public Long insertData(String tgl, String jmark, String qshalat, int cshalat, int cpuasa){
         myDB = getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("id_tgl", tgl);
+        values.put("jenismark", jmark);
+        values.put("waktusholat", qshalat);
+        values.put("centangshalat", cshalat);
+        values.put("centangpuasa", cpuasa);
 
+        //tabel utama
         long rowId = myDB.insert(DB_TABLE_UTAMA, null, values);
+
+        // Insert ke tabel HAID atau ISTIHADHAH jika sesuai
+        if ("HAID".equalsIgnoreCase(jmark)) {
+            ContentValues haidValues = new ContentValues();
+            haidValues.put("id_tgl", tgl);
+            haidValues.put("jenismark", jmark);
+            myDB.insert(DB_TABLE_HAID, null, haidValues);
+        } else if ("ISTIHADHAH".equalsIgnoreCase(jmark)) {
+            ContentValues istihadhahValues = new ContentValues();
+            istihadhahValues.put("id_tgl", tgl);
+            istihadhahValues.put("jenismark", jmark);
+            myDB.insert(DB_TABLE_ISTIHADHAH, null, istihadhahValues);
+        }
 
         return rowId;
     }
 
-    public void editDataEntry(String tgl){
+    public void editDataEntry(String tgl, String jmark, String qshalat, int cshalat, int cpuasa){
         ContentValues values = new ContentValues();
         values.put("id_tgl", tgl);
+        values.put("jenismark", jmark);
+        values.put("waktusholat", qshalat);
+        values.put("centangshalat", cshalat);
+        values.put("centangpuasa", cpuasa);
 
         myDB = getReadableDatabase();
         myDB.update(DB_TABLE_UTAMA, values, "id_tgl="+tgl, null);
--- app/src/main/java/com/galuhsukma/kalendernya/Markday.java
+++ app/src/main/java/com/galuhsukma/kalendernya/Markday.java
@@ -4,19 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.jar.JarEntry;
+
 public class Markday extends AppCompatActivity {
     DatabaseHelper myDb;
     private TextView dayselected;
-    private String selected_date_string, selected_monthYear_string, tgl_database;
+    private String selected_date_string, selected_monthYear_string, tgl_database, jenismark, qadhashalat;
+    private int centangshalat, centangpuasa;
+    private CardView cvmhaid, cvmistihadhah, cvsubuh, cvdhuhur, cvashar, cvmaghrib, cvisha;
+    private CheckBox cbshalat, cbpuasa;
     Button simpanbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +39,86 @@ public class Markday extends AppCompatActivity {
         tgl_database = intent.getStringExtra("tgldatabase");
 
         dayselected = findViewById(R.id.dayselected);
+        cvmhaid = findViewById(R.id.cvmarkhaid);
+        cvmistihadhah = findViewById(R.id.cvmarkistihadhah);
+        cvsubuh = findViewById(R.id.cvsubuh);
+        cvdhuhur = findViewById(R.id.cvdhuhur);
+        cvashar = findViewById(R.id.cvashar);
+        cvmaghrib = findViewById(R.id.cvmaghrib);
+        cvisha = findViewById(R.id.cvisha);
+        cbshalat = findViewById(R.id.cekboxshalat);
+        cbpuasa = findViewById(R.id.cekboxpuasa);
         simpanbtn = findViewById(R.id.btnsimpan);
 
+        //nampilin tanggal
         dayselected.setText(selected_date_string+" "+selected_monthYear_string);
 
+        //pilih jenis mark tanggal (H/I)
+        cvmhaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jenismark = "HAID";
+            }
+        });
+        cvmistihadhah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jenismark = "ISTIHADHAH";
+            }
+        });
+
+        //pilih sholat (gambar)
+        cvsubuh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qadhashalat = "Shalat Subuh";
+            }
+        });
+        cvdhuhur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qadhashalat = "Shalat Dhuhur";
+            }
+        });
+        cvashar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qadhashalat = "Shalat Ashar";
+            }
+        });
+        cvmaghrib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qadhashalat = "Shalat Maghrib";
+            }
+        });
+        cvisha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qadhashalat = "Shalat Isha'";
+            }
+        });
+
+        //centangin sholat puasa
+        if (cbshalat.isChecked()) {
+            centangshalat = 1;
+        } else {
+            centangshalat = 0;
+        }
+
+        if (cbpuasa.isChecked()) {
+            centangpuasa = 1;
+        } else {
+            centangpuasa = 0;
+        }
+
+
+        //tombol simpan data
         simpanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                myDb.insertData(tgl_database);
+                myDb.insertData(tgl_database, jenismark, qadhashalat, centangshalat, centangpuasa);
                 Toast.makeText(Markday.this,"Data Saved Successfully", Toast.LENGTH_SHORT).show();
-
             }
         });
     }
